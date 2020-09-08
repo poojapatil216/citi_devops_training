1bc072e1d0ba9bc007490e49bff8256ae8a595db
pooja <poojapatil.216@gmail.com>
2020-09-08 11:27:35 +0000

    print 7 times

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		int i;
-		for(i = 0; i <= 5; i++)
+		for(i = 0; i <= 7; i++)
 		{
 			System.out.println("\n Hello World ");
 		}	
